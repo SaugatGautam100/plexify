c8c50dfc3e214516b1ed67232f94c369c1db56d9
Sachendra-2000 <shresthasachendra8@gmail.com>
2025-07-09 18:29:03 +0545

    Remove disabled middleware file

--- middleware.js
+++
@@ -1,9 +0,0 @@
-// Middleware disabled - all pages are now accessible without authentication
-// export { default } from "next-auth/middleware";
-
-// export const config = { matcher:
-//      [
-//        "/checkout",
-//        "/profile"
-//     ] 
-//     };
