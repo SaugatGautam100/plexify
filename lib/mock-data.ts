be6e4ccce7f892e5fec526dd314b21bdf65a6ced
SaugatGautam100 <gautamsaugat100@gmail.com>
2025-07-31 11:48:49 +0545

    updated image link for featured products

--- lib/mock-data.ts
+++ lib/mock-data.ts
@@ -171,10 +171,10 @@ export const products: Product[] = [
     productCategory: "Dry Fruits Masala & Oil",
     productId: "Zq0Dit4xBl1JEKUXOeNc53S1j",
     productImageUris: [
-"https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/suPbGytrYMSTPyPaPowTK2uQ3gi1%2Fimages%2F411745b3-c054-42a2-bf7f-33c9603227a6?alt=media&token=5fb8640d-026b-4ce7-af40-fde2aaf0de51",
-"https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/suPbGytrYMSTPyPaPowTK2uQ3gi1%2Fimages%2F917abe7a-23f7-4514-ad14-dfccf260ea77?alt=media&token=12ade941-637a-4cce-bcfc-9c3452561af0",
-"https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/suPbGytrYMSTPyPaPowTK2uQ3gi1%2Fimages%2F19b843a5-735c-4e5e-b96a-78aa3d84fda1?alt=media&token=2a3caef3-fb48-493f-8cda-55319f527ab9",
-"https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/suPbGytrYMSTPyPaPowTK2uQ3gi1%2Fimages%2F17ed9c0d-0756-4da6-81e8-062f07fb21ed?alt=media&token=c624327a-2064-4948-a545-468e18411ee1"
+"https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/2Ed8DN4Yw7RfSZnP95jz2FHJ4vt1%2Fimages%2F500e0d7e-811f-4152-930d-ef3b3e7a369e?alt=media&token=abc82aa2-9994-47fb-9416-5f685ad0e28e",
+"https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/2Ed8DN4Yw7RfSZnP95jz2FHJ4vt1%2Fimages%2F3584fe67-233c-4d05-9244-6748e9787763?alt=media&token=bac55218-e743-4275-8db8-885e9b88dd01",
+"https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/2Ed8DN4Yw7RfSZnP95jz2FHJ4vt1%2Fimages%2F866600e1-d610-4250-bb45-a0ee085795dd?alt=media&token=e83a2bcc-b0d5-4fd0-a793-c03c3c547543",
+"https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/2Ed8DN4Yw7RfSZnP95jz2FHJ4vt1%2Fimages%2Fd75e0b54-aa47-4d45-a94a-ed74c6685a5f?alt=media&token=02d40e8e-0a92-4577-b52f-25987e5fff2d"
     ],
     productPrice: 240,
     productQuantity: 1,
@@ -190,7 +190,7 @@ export const products: Product[] = [
     productCategory: "Dairy & Breakfast",
     productId: "WExzVbdE7X5UYkM763k9VraO5",
     productImageUris: [
-      'https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/suPbGytrYMSTPyPaPowTK2uQ3gi1%2Fimages%2F204a60dc-3c70-460c-ac47-ea6426495397?alt=media&token=b9ff352b-97f0-4faa-a256-ee3f3a112f87'
+      'https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/2Ed8DN4Yw7RfSZnP95jz2FHJ4vt1%2Fimages%2F5642e0f0-2024-4b2b-b211-30216ff57dfa?alt=media&token=f3af9ffd-adc2-4098-97c8-46337d79f819'
     ],
     productPrice: 125,
     productQuantity: 1,
@@ -206,7 +206,7 @@ export const products: Product[] = [
     productCategory: "Atta Rice & Dal",
     productId: "2JDKrAHHELFI3L5thBICth4RC",
     productImageUris: [
-      'https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/suPbGytrYMSTPyPaPowTK2uQ3gi1%2Fimages%2F7f582519-69e1-4830-8682-8bf08ccb50ea?alt=media&token=5161bdf3-4089-4eb8-bbf1-d3e616c5905c'
+      'https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/2Ed8DN4Yw7RfSZnP95jz2FHJ4vt1%2Fimages%2Ff525a6b5-1ad3-43b1-add9-3de266bc36b2?alt=media&token=98652298-eb4f-4263-b508-4d77ab218e1b'
     ],
     productPrice: 130,
     productQuantity: 1,
@@ -222,7 +222,7 @@ export const products: Product[] = [
     productCategory: "Atta Rice & Dal",
     productId: "yK3nwUFsF2UcTkbUDDl4ROYLI",
     productImageUris: [
-      'https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/suPbGytrYMSTPyPaPowTK2uQ3gi1%2Fimages%2F8f0dbbac-666a-43b0-9574-449dbdb1a33f?alt=media&token=2ca3984e-f590-4cb1-9ee0-851f517061a6'
+      'https://firebasestorage.googleapis.com/v0/b/plexify-6469a.firebasestorage.app/o/2Ed8DN4Yw7RfSZnP95jz2FHJ4vt1%2Fimages%2F8018beda-4bd5-4e55-8412-db5b07313b93?alt=media&token=ebf35ae9-57b9-4191-8255-6d96133e7c50'
     ],
     productPrice: 150,
     productQuantity: 1,
